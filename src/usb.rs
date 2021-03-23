3f2dbeda03de3d9442641be822d75c9db4bd35b1
Kevin Lannen <kevin@patroness.com>
2021-03-22 21:44:08 -0600

    Not sure

--- src/usb.rs
+++ src/usb.rs
@@ -3,7 +3,7 @@
 //! Requires the `stm32-usbd` feature
 
 // use crate::{gpio::{Analog, Output}};
-use crate::{stm32::{RCC, USB_FS_DEVICE}};
+use crate::{stm32::{RCC, USB}};
 use stm32_usbd::UsbPeripheral;
 
 // use crate::gpio::gpioa::{PA11, PA12};
@@ -15,7 +15,7 @@ pub use stm32_usbd::UsbBus;
 /// then gets passed to the [`UsbBus`].
 pub struct Peripheral {
     /// USB Register Block
-    pub usb: USB_FS_DEVICE,
+    pub usb: USB,
     // /// Data Negative Pin
     // pub pin_dm: PA11<Output<Analog>>,
     // /// Data Positiv Pin
@@ -25,7 +25,7 @@ pub struct Peripheral {
 unsafe impl Sync for Peripheral {}
 
 unsafe impl UsbPeripheral for Peripheral {
-    const REGISTERS: *const () = USB_FS_DEVICE::ptr() as *const ();
+    const REGISTERS: *const () = USB::ptr() as *const ();
     const DP_PULL_UP_FEATURE: bool = true;
     const EP_MEMORY: *const () = 0x4000_6000 as _;
     const EP_MEMORY_SIZE: usize = 1024;
